1a37c4f006ff73adc01e8441cf548d9917cfb405
Benjamin Blattberg <benjamin.blattberg@gmail.com>
2015-01-06 16:59:15 -0600

    pre Lesson 7

--- lib/kv/registry.ex
+++ lib/kv/registry.ex
@@ -6,73 +6,63 @@ defmodule KV.Registry do
   @doc """
   Starts the registry.
   """
-  def start_link(event_manager, buckets, opts \\ []) do
-    # 1. Pass the buckets supervisor as argument
-    GenServer.start_link(__MODULE__, {event_manager, buckets}, opts)
+  def start_link(table, event_manager, buckets, opts \\ []) do
+    # 1. We now expect the table as argument and pass it to the server
+    GenServer.start_link(__MODULE__, {table, event_manager, buckets}, opts)
   end
 
   @doc """
-  Looks up the bucket pid for `name` stored in `server`.
+  Looks up the bucket pid for `name` stored in `table`.
 
-  Returns `{:ok, pid}` if the bucket exists, `:error` otherwise.
+  Returns `{:ok, pid}` if a bucket exists, `:error` otherwise.
   """
-  def lookup(server, name) do
-    GenServer.call(server, {:lookup, name})
-  end
-
-  @doc """
-  Ensures there is a bucket associated to the given `name` in `server`.
-  """
-  def create(server, name) do
-    GenServer.cast(server, {:create, name})
-  end
-
-  @doc """
-  Stops the registry.
-  """
-  def stop(server) do
-    GenServer.call(server, :stop)
-  end
-
-  ## Server Callbacks
-
-  def init({events, buckets}) do
-    names = HashDict.new
-    refs  = HashDict.new
-    # 2. Store the buckets supervisor in the state
-    {:ok, %{names: names, refs: refs, events: events, buckets: buckets}}
-  end
-
-  def handle_call(:stop, _from, state) do
-    {:stop, :normal, :ok, state}
-  end
-
-  def handle_call({:lookup, name}, _from, state) do
-    {:reply, HashDict.fetch(state.names, name), state}
-  end
-
-  def handle_cast({:create, name}, state) do
-    if HashDict.get(state.names, name) do
-      {:noreply, state}
-    else
-      # 3. Use the buckets supervisor instead of starting buckets directly
-      {:ok, pid} = KV.Bucket.Supervisor.start_bucket(state.buckets)
-      ref = Process.monitor(pid)
-      refs = HashDict.put(state.refs, ref, name)
-      names = HashDict.put(state.names, name, pid)
-      GenEvent.sync_notify(state.events, {:create, name, pid})
-      {:noreply, %{state | names: names, refs: refs}}
+  def lookup(table, name) do
+    # 2. lookup now expects a table and looks directly into ETS.
+    #    No request is sent to the server.
+    case :ets.lookup(table, name) do
+      [{^name, bucket}] -> {:ok, bucket}
+      [] -> :error
     end
   end
 
-  # handle_cast/2 must be used for asynchronous requests, when you don't care about a reply. A cast does not even guarantee the server has received the message and, for this reason, must be used sparingly. For example, the create/2 function we have defined in this chapter should have used call/2. We have used cast/2 for didactic purposes.
+  @doc """
+  Ensures there is a bucket associated with the given `name` in `server`.
+  """
+  def create(server, name) do
+    GenServer.call(server, {:create, name})
+  end
+
+  ## Server callbacks
+
+  def init({table, events, buckets}) do
+    refs = :ets.foldl(fn {name, pid}, acc ->
+      HashDict.put(acc, Process.monitor(pid), name)
+    end, HashDict.new, table)
+
+    {:ok, %{names: table, refs: refs, events: events, buckets: buckets}}
+  end
+
+  # 4. The previous handle_call callback for lookup was removed
+  def handle_call({:create, name}, _from, state) do
+    case lookup(state.names, name) do
+      {:ok, pid} ->
+        {:reply, pid, state} # Reply with pid
+      :error ->
+        {:ok, pid} = KV.Bucket.Supervisor.start_bucket(state.buckets)
+        ref = Process.monitor(pid)
+        refs = HashDict.put(state.refs, ref, name)
+        :ets.insert(state.names, {name, pid})
+        GenEvent.sync_notify(state.events, {:create, name, pid})
+        {:reply, pid, %{state | refs: refs}} # Reply with pid
+    end
+  end
 
   def handle_info({:DOWN, ref, :process, pid, _reason}, state) do
+    # 6. Delete from the ETS table instead of the HashDict
     {name, refs} = HashDict.pop(state.refs, ref)
-    names = HashDict.delete(state.names, name)
-    # 4. Push a notification to the event manager on exit
+    :ets.delete(state.names, name)
     GenEvent.sync_notify(state.events, {:exit, name, pid})
-    {:noreply, %{state | names: names, refs: refs}}
+    {:noreply, %{state | refs: refs}}
   end
 
   def handle_info(_msg, state) do
--- test/kv/registry_test.exs
+++ test/kv/registry_test.exs
@@ -11,46 +11,70 @@ defmodule KV.RegistryTest do
   end
 
   setup do
-    {:ok, sup} = KV.Bucket.Supervisor.start_link
-    {:ok, manager} = GenEvent.start_link
-    {:ok, registry} = KV.Registry.start_link(manager, sup)
-
-    GenEvent.add_mon_handler(manager, Forwarder, self())
-    {:ok, registry: registry}
+    ets = :ets.new(:registry_table, [:set, :public])
+    registry = start_registry(ets)
+    {:ok, registry: registry, ets: ets}
   end
 
-  test "removes bucket on crash", %{registry: registry} do
+  defp start_registry(ets) do
+    {:ok, sup} = KV.Bucket.Supervisor.start_link
+    {:ok, manager} = GenEvent.start_link
+    {:ok, registry} = KV.Registry.start_link(ets, manager, sup)
+
+    GenEvent.add_mon_handler(manager, Forwarder, self())
+    registry
+  end
+
+  test "removes bucket on crash", %{registry: registry, ets: ets} do
     KV.Registry.create(registry, "shopping")
-    {:ok, bucket} = KV.Registry.lookup(registry, "shopping")
+    {:ok, bucket} = KV.Registry.lookup(ets, "shopping")
 
     # Kill the bucket and wait for the notification
     Process.exit(bucket, :shutdown)
     assert_receive {:exit, "shopping", ^bucket}
-    assert KV.Registry.lookup(registry, "shopping") == :error
+    assert KV.Registry.lookup(ets, "shopping") == :error
   end
 
-  test "sends events on create and crash", %{registry: registry} do
+  test "sends events on create and crash", %{registry: registry, ets: ets} do
     KV.Registry.create(registry, "shopping")
-    {:ok, bucket} = KV.Registry.lookup(registry, "shopping")
+    {:ok, bucket} = KV.Registry.lookup(ets, "shopping")
     assert_receive {:create, "shopping", ^bucket}
 
     Agent.stop(bucket)
     assert_receive {:exit, "shopping", ^bucket}
   end
-  test "spawns buckets", %{registry: registry} do
-    assert KV.Registry.lookup(registry, "shopping") == :error
+  test "spawns buckets", %{registry: registry, ets: ets} do
+    assert KV.Registry.lookup(ets, "shopping") == :error
 
     KV.Registry.create(registry, "shopping")
-    assert {:ok, bucket} = KV.Registry.lookup(registry, "shopping")
+    assert {:ok, bucket} = KV.Registry.lookup(ets, "shopping")
 
     KV.Bucket.put(bucket, "milk", 1)
     assert KV.Bucket.get(bucket, "milk") == 1
   end
 
-  test "removes buckets on exit", %{registry: registry} do
+  test "removes buckets on exit", %{registry: registry, ets: ets} do
     KV.Registry.create(registry, "shopping")
-    {:ok, bucket} = KV.Registry.lookup(registry, "shopping")
+    {:ok, bucket} = KV.Registry.lookup(ets, "shopping")
     Agent.stop(bucket)
-    assert KV.Registry.lookup(registry, "shopping") == :error
+    assert_receive {:exit, "shopping", ^bucket} # Wait for event
+    assert KV.Registry.lookup(ets, "shopping") == :error
+  end
+
+  test "monitors existing entries", %{registry: registry, ets: ets} do
+    bucket = KV.Registry.create(registry, "shopping")
+
+    # Kill the registry. We unlink first, otherwise it will kill the test
+    Process.unlink(registry)
+    Process.exit(registry, :shutdown)
+
+    # Start a new registry with the existing table and access the bucket
+    start_registry(ets)
+    assert KV.Registry.lookup(ets, "shopping") == {:ok, bucket}
+
+    # Once the bucket dies, we should receive notifications
+    Process.exit(bucket, :shutdown)
+    assert_receive {:exit, "shopping", ^bucket}
+    assert KV.Registry.lookup(ets, "shopping") == :error
   end
 end
