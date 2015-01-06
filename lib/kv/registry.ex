395f2781fe442f20e45a48aad703913460571a91
Benjamin Blattberg <benjamin.blattberg@gmail.com>
2015-01-06 15:06:58 -0600

    Pre lesson 5

---
+++ lib/kv/registry.ex
@@ -0,0 +1,84 @@
+defmodule KV.Registry do
+  use GenServer
+
+  ## Client API
+
+  @doc """
+  Starts the registry.
+  """
+  def start_link(event_manager, opts \\ []) do
+    # 1. start_link now expects the event manager as argument
+    GenServer.start_link(__MODULE__, event_manager, opts)
+  end
+
+  @doc """
+  Looks up the bucket pid for `name` stored in `server`.
+
+  Returns `{:ok, pid}` if the bucket exists, `:error` otherwise.
+  """
+  def lookup(server, name) do
+    GenServer.call(server, {:lookup, name})
+  end
+
+  @doc """
+  Ensures there is a bucket associated to the given `name` in `server`.
+  """
+  def create(server, name) do
+    GenServer.cast(server, {:create, name})
+  end
+
+  @doc """
+  Stops the registry.
+  """
+  def stop(server) do
+    GenServer.call(server, :stop)
+  end
+
+  ## Server Callbacks
+
+  def init(events) do
+    # 2. The init callback now receives the event manager.
+    #    We have also changed the manager state from a tuple
+    #    to a map, allowing us to add new fields in the future
+    #    without needing to rewrite all callbacks.
+    names = HashDict.new
+    refs  = HashDict.new
+    {:ok, %{names: names, refs: refs, events: events}}
+  end
+
+  def handle_call(:stop, _from, state) do
+    {:stop, :normal, :ok, state}
+  end
+
+  def handle_call({:lookup, name}, _from, state) do
+    {:reply, HashDict.fetch(state.names, name), state}
+  end
+
+  def handle_cast({:create, name}, state) do
+    if HashDict.get(state.names, name) do
+      {:noreply, state}
+    else
+      {:ok, pid} = KV.Bucket.start_link()
+      ref = Process.monitor(pid)
+      refs = HashDict.put(state.refs, ref, name)
+      names = HashDict.put(state.names, name, pid)
+      # 3. Push a notification to the event manager on create
+      GenEvent.sync_notify(state.events, {:create, name, pid})
+      {:noreply, %{state | names: names, refs: refs}}
+    end
+  end
+
+  # handle_cast/2 must be used for asynchronous requests, when you don't care about a reply. A cast does not even guarantee the server has received the message and, for this reason, must be used sparingly. For example, the create/2 function we have defined in this chapter should have used call/2. We have used cast/2 for didactic purposes.
+
+  def handle_info({:DOWN, ref, :process, pid, _reason}, state) do
+    {name, refs} = HashDict.pop(state.refs, ref)
+    names = HashDict.delete(state.names, name)
+    # 4. Push a notification to the event manager on exit
+    GenEvent.sync_notify(state.events, {:exit, name, pid})
+    {:noreply, %{state | names: names, refs: refs}}
+  end
+
+  def handle_info(_msg, state) do
+    {:noreply, state}
+  end
+end
