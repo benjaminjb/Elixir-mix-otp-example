02883963dac1ffff2e9a34cd474e3ecaa95f84bf
Benjamin Blattberg <benjamin.blattberg@gmail.com>
2015-01-06 16:30:31 -0600

    Pre lesson 6: ETS

--- lib/kv/registry.ex
+++ lib/kv/registry.ex
@@ -6,9 +6,9 @@ defmodule KV.Registry do
   @doc """
   Starts the registry.
   """
-  def start_link(event_manager, opts \\ []) do
-    # 1. start_link now expects the event manager as argument
-    GenServer.start_link(__MODULE__, event_manager, opts)
+  def start_link(event_manager, buckets, opts \\ []) do
+    # 1. Pass the buckets supervisor as argument
+    GenServer.start_link(__MODULE__, {event_manager, buckets}, opts)
   end
 
   @doc """
@@ -36,14 +36,11 @@ defmodule KV.Registry do
 
   ## Server Callbacks
 
-  def init(events) do
-    # 2. The init callback now receives the event manager.
-    #    We have also changed the manager state from a tuple
-    #    to a map, allowing us to add new fields in the future
-    #    without needing to rewrite all callbacks.
+  def init({events, buckets}) do
     names = HashDict.new
     refs  = HashDict.new
-    {:ok, %{names: names, refs: refs, events: events}}
+    # 2. Store the buckets supervisor in the state
+    {:ok, %{names: names, refs: refs, events: events, buckets: buckets}}
   end
 
   def handle_call(:stop, _from, state) do
@@ -58,11 +55,11 @@ defmodule KV.Registry do
     if HashDict.get(state.names, name) do
       {:noreply, state}
     else
-      {:ok, pid} = KV.Bucket.start_link()
+      # 3. Use the buckets supervisor instead of starting buckets directly
+      {:ok, pid} = KV.Bucket.Supervisor.start_bucket(state.buckets)
       ref = Process.monitor(pid)
       refs = HashDict.put(state.refs, ref, name)
       names = HashDict.put(state.names, name, pid)
-      # 3. Push a notification to the event manager on create
       GenEvent.sync_notify(state.events, {:create, name, pid})
       {:noreply, %{state | names: names, refs: refs}}
     end
--- test/kv/registry_test.exs
+++ test/kv/registry_test.exs
@@ -11,11 +11,22 @@ defmodule KV.RegistryTest do
   end
 
   setup do
+    {:ok, sup} = KV.Bucket.Supervisor.start_link
     {:ok, manager} = GenEvent.start_link
-    {:ok, registry} = KV.Registry.start_link(manager)
+    {:ok, registry} = KV.Registry.start_link(manager, sup)
 
     GenEvent.add_mon_handler(manager, Forwarder, self())
     {:ok, registry: registry}
+  end
+
+  test "removes bucket on crash", %{registry: registry} do
+    KV.Registry.create(registry, "shopping")
+    {:ok, bucket} = KV.Registry.lookup(registry, "shopping")
+
+    # Kill the bucket and wait for the notification
+    Process.exit(bucket, :shutdown)
+    assert_receive {:exit, "shopping", ^bucket}
+    assert KV.Registry.lookup(registry, "shopping") == :error
   end
 
   test "sends events on create and crash", %{registry: registry} do
